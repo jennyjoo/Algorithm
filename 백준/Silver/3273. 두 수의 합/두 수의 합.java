01c8503e0e3ca2cba140d450a9f6fd3bc98c9ea8
jennyjoo <165225479+jennyjoo@users.noreply.github.com>
2024-08-13 21:58:32 +0900

    [Silver III] Title: 두 수의 합, Time: 404 ms, Memory: 26644 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -3,45 +3,65 @@ import java.util.*;
 
 public class Main {
 
-   public int solution(int[] components, int m){
-       int lt = 0;
-       int rt = components.length - 1;
-       int cnt = 0;
 
-       while(lt < rt){
-           int first = components[lt];
-           int second = components[rt];
+    public int solution(int[] numbers, int target){
 
-           if(first + second < m){
-               lt++;
-           }else{
-               if(first + second == m){
-                   lt++; rt--;
-                   cnt++;
-               }
-               else{
-                   rt--;
-               }
-           }
-       }
+        /*
+        변수
+         */
+        int lt = 0, rt = numbers.length - 1;
+        int answer = 0;
 
-       return cnt;
-   }
+        
+        /*
+        이분탐색
+         */
+        while(lt < rt){
+            int a = numbers[lt];
+            int b = numbers[rt];
+
+            if(a + b == target ){ //합이 target
+                answer += 1;
+                lt += 1;
+                rt -= 1;
+            }
+            else if(a + b < target){ //target보다 작을 때
+                lt += 1;
+            }
+            else { // 클 때
+                rt -= 1;
+            }
+        }
+
+        
+        /*
+        정답
+         */
+        return answer;
+    }
 
     public static void main(String[] args) throws IOException {
         Main T = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        /*
+        입력부
+         */
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] components = new int[n];
-        for(int i = 0; i < n; components[i++] = Integer.parseInt(st.nextToken())){}
 
-        int m = Integer.parseInt(br.readLine());
+        int[] numbers = new int[n];
+        for(int i = 0; i < n; numbers[i++] = Integer.parseInt(st.nextToken())){}
 
-        Arrays.sort(components);
-        System.out.println(T.solution(components, m));
+        int target = Integer.parseInt(br.readLine());
+        Arrays.sort(numbers);
+
+        
+        /*
+        main, 출력부
+         */
+        System.out.println(T.solution(numbers, target));
 
     }
 }
