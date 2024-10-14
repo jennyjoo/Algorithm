2451cd920f01f0e9ab8bf084855e1986c8795442
정연주 <165225479+jennyjoo@users.noreply.github.com>
2024-10-14 16:43:12 +0900

    [Bronze I] Title: 이항 계수 1, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -3,25 +3,21 @@ import java.util.*;
 
 public class Main {
 
-    private Integer solution(int n, int k){
-        int answer = 0;
-
-        n = n + 1;
-        int[][] temp = new int[n][n];
-        for(int i = 0; i < n; i++){
-            temp[i][0] = 1;
-            temp[i][i] = 1;
+    static int count = 0;
+    static boolean[] visited;
+    private void solution(int k, int start){
+        if(k == 0) {
+            count++;
+            return;
         }
 
-        for(int i = 2; i < n; i++){
-            for(int j = 1; j < n - 1; j++){
-                temp[i][j] = temp[i-1][j-1] + temp[i-1][j];
+        for(int i = start; i < visited.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                solution(k -1, i + 1);
+                visited[i] = false;
             }
         }
-
-        answer = temp[n - 1][k];
-
-        return answer;
     }
 
     public static void main(String[] args) throws IOException {
@@ -33,9 +29,10 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        System.out.println(T.solution(N, K));
+        visited = new boolean[N];
+
+        T.solution(K, 0);
+        System.out.println(count);
 
     }
-
-
 }
