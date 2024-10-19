b37046a6bfa4ea8579853191e6b631ee8f0985b6
정연주 <165225479+jennyjoo@users.noreply.github.com>
2024-10-20 03:15:41 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,56 +1,93 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    static int N, M, r, c, d;
-    static int[][] arr;
+
+    static int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
+    static int[][] room;
+    static int N, M; //세로 가로
     static int count = 1;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    static void DFS(int x, int y, int direction){
 
-        N = scan.nextInt();
-        M = scan.nextInt();
-        r = scan.nextInt();
-        c = scan.nextInt();
-        d = scan.nextInt();
+        room[y][x] = 2;
 
-        arr = new int[N][M];
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                arr[i][j] = scan.nextInt();
-            }
-        }
+//        for(int[] ss : room){
+//            for(int s : ss){
+//                System.out.print(s + " ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+        
+        int newDir = direction;
+        for(int d = 0; d < 4; d++){
+            newDir = (newDir + 3)%4;
+            int nx = x + dx[newDir];
+            int ny = y + dy[newDir];
 
-        clean(r, c, d);
-        System.out.println(count);
-    }
+            if(nx >= 0 && ny >= 0 && nx < M && ny < N && room[ny][nx] != 1){
+                if(room[ny][nx] == 0){
 
-    public static void clean(int x, int y, int dir) {
+                    count += 1;
 
-        arr[x][y] = -1;
-
-        for(int i = 0; i < 4; i++) {
-            dir = (dir+3)%4;
-
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
-            if(nx >= 0 && ny >= 0 && nx < N && ny < M) {
-                if(arr[nx][ny] == 0) {
-                    count++;
-                    clean(nx, ny, dir);
+                    DFS(nx, ny, newDir);
                     return;
                 }
             }
         }
 
-        int d = (dir + 2) % 4; 
-        int bx = x + dx[d];
-        int by = y + dy[d];
-        if(bx >= 0 && by >= 0 && bx < N && by < M && arr[bx][by] != 1) {
-            clean(bx, by, dir); 
+
+        int opposite = (direction + 2)%4;
+        int bx = x + dx[opposite];
+        int by = y + dy[opposite];
+
+        if(room[by][bx] == 1) return;
+        if(bx >= 0 && by >= 0 && bx < M && by < N && room[by][bx] != 1) DFS(bx, by, direction);
+    }
+
+
+
+    private void solution(int x, int y, int d){
+
+        if(room[y][x] == 1) {
+            System.out.println(0);
+            return;
         }
+
+        DFS(x, y, d);
+        System.out.println(count);
+    }
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); //세로
+        M = Integer.parseInt(st.nextToken()); //가로
+
+        room = new int[N][M];
+
+
+
+        st = new StringTokenizer(br.readLine());
+        int robotY, robotX, robotDir;
+        robotY = Integer.parseInt(st.nextToken());
+        robotX = Integer.parseInt(st.nextToken());
+        robotDir = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                room[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        T.solution(robotX, robotY, robotDir);
+
     }
 }
