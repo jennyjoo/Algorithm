d4d2e8de8eef69721a93c25249d3d901a1662489
정연주 <165225479+jennyjoo@users.noreply.github.com>
2024-10-27 23:25:14 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String in = br.readLine();
+        System.out.println(in + "??!");
+
+    }
+}
