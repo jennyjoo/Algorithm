d12592e558d301e0838f21ad91c372b6658d671c
정연주 <165225479+jennyjoo@users.noreply.github.com>
2024-10-29 22:18:50 +0900

    [Gold IV] Title: 케이크 자르기, Time: 180 ms, Memory: 14908 KB -BaekjoonHub

--- 백준/Gold/17179. 케이크 자르기/케이크 자르기.java
+++ 백준/Gold/17179. 케이크 자르기/케이크 자르기.java
@@ -1,50 +1,62 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int ans = 0;
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int l = Integer.parseInt(st.nextToken());
-        int[] arr = new int[m + 1];
+    private void solution(int cutTimes, int length, int[] points){
 
-        for (int i = 0; i < m; ++i) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        arr[m] = l;
+        int lt = 1;
+        int rt = length;
+        int mid;
 
-        for (int i = 0; i < n; ++i) {
-            int num = Integer.parseInt(br.readLine());
-            int left = 0;
-            int right = l;
-            while (left <= right) {
-                int cnt = 0;
-                int prev = 0;
-                int mid = (left + right) / 2;
-                for (int j = 0; j <= m; ++j) {
-                    if (arr[j] - prev >= mid) {
-                        ++cnt;
-                        prev = arr[j];
-                    }
-                }
-                if (cnt > num) {
-                    left = mid + 1;
-                    ans = Math.max(ans, mid);
-                } else {
-                    right = mid - 1;
+        while(lt < rt){
+            mid = (lt + rt)/2;
+
+            int count = 0;
+
+            int last = 0;
+            for(int j = 0; j < points.length; j++){
+                if(points[j] - last >= mid){
+                    count++;
+                    last = points[j];
                 }
             }
-            bw.write(right + "\n");
+            if(count > cutTimes){
+                lt = mid + 1;
+            }
+            else{
+                rt = mid;
+            }
         }
-        bw.close();
+
+        System.out.println(lt-1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] points = new int[M + 1];
+        int[] arr = new int[N];
+
+        for(int i = 0; i < M; i++){
+            points[i] = Integer.parseInt(br.readLine());
+        }
+        points[M] = L;
+
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for(int i = 0; i < N; i++){
+            T.solution(arr[i], L, points);
+        }
     }
 }
