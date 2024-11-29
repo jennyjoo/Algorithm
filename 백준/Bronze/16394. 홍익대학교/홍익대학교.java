bae157b9736d30171fcee43ae2847c68cb6cb713
정연주 <165225479+jennyjoo@users.noreply.github.com>
2024-11-29 23:14:01 +0900

    [Bronze V] Title: 홍익대학교, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        System.out.println(year -1946);
+
+    }
+}
